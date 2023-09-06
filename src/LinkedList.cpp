81f829190ab518d51fa82fec123b70575cbd941e
Ragavendra <raga@example.com>
2023-09-06 15:19:56 -0700

    lbne ebrop yvere tass

---
+++ src/LinkedList.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+
+// #include <string>
+
+using namespace std;
+
+struct LinkList
+{
+	int no;
+	LinkList *next;
+} one, three;
+
+class LinkList_
+{
+
+private:
+	LinkList *_linkList = NULL;
+
+public:
+	void InsertEleTail(int num)
+	{
+		// if first ele
+		if (_linkList == NULL)
+		{
+			InsertFirst(num);
+			return;
+		}
+
+		// LinkList *linkList = new LinkList();
+		LinkList *linkList = _linkList;
+		while (linkList->next != NULL)
+		{
+			linkList = linkList->next;
+		}
+
+		LinkList *newEle = new LinkList();
+		newEle->no = num;
+		// newEle->next_ = NULL;
+		linkList->next = newEle;
+		// _linkList->next_ = linkList;
+	}
+
+	void InsertFirst(int num)
+	{
+		if (_linkList == NULL)
+		{
+			_linkList = new LinkList();
+			_linkList->no = num;
+		}
+	}
+
+	void InsertEleHead(int num)
+	{
+		// if first ele
+		if (_linkList == NULL)
+		{
+			InsertFirst(num);
+			return;
+		}
+
+		// store ele in first
+		LinkList *newEle = new LinkList();
+		newEle->no = num;
+		newEle->next = _linkList;
+		_linkList = newEle;
+	}
+
+	void DisplayList()
+	{
+		while (_linkList != NULL)
+		{
+			cout << _linkList->no << " ";
+			_linkList = _linkList->next;
+		}
+	}
+};
+
+int main()
+{
+
+	LinkList_ linkList;
+
+	linkList.InsertEleTail(10);
+	linkList.InsertEleTail(20);
+	linkList.InsertEleTail(60);
+	// linkList.DisplayList();
+	linkList.InsertEleHead(25);
+	linkList.InsertEleHead(65);
+	linkList.DisplayList();
+}
---
+++ src/some.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <fstream>
+
+// #include <string>
+
+using namespace std;
+
+void SwapNos(int &a, int &b)
+{
+	int c = b;
+	b = a;
+	a = c;
+}
+
+enum Movement
+{
+	Left,
+	Right,
+	Up,
+	Down
+};
+
+class Fish
+{
+	public:
+		int length;
+		int height;
+		string Move(Movement movn);
+		Fish(int l, int h, int w);
+
+		Fish()
+		{}
+
+	private:
+		int weight;
+
+	protected:
+		string shape;
+};
+
+string Fish::Move(Movement mov)
+{
+	switch(mov)
+	{
+		case Left:
+			return "Left";
+			break;
+
+		case Right:
+			return "Right";
+			break;
+
+		default:
+			return "Unknown movement";
+	}
+}
+
+Fish::Fish(int l, int h, int w)
+{
+	length = l;
+	height = h;
+	weight = w;
+}
+
+class Cichlid : public Fish
+{
+	public:
+		void Cichlid_(int l, int h)
+		{
+			length = l;
+			height = h;
+		}
+
+		void setLength(int l)
+		{
+			length = l;
+			// weight = 3;
+			shape = "pointed";
+		}
+
+};
+
+int main()
+{
+	std::cout << "Learn cpp ";
+
+	char s = 'F';
+	char l = 66;
+	string str = "Str";
+	str.append("str");
+
+	cout << s << endl;
+	cout << l << endl;
+	cout << str;
+	// getline(cin, str);
+	// cout << "Str is " << str;
+	cout << sqrt(64);
+
+	// int arr[3] = { 6, 8, 1 };
+	int arr[] = { 6, 8, 1 };
+
+	for(int ele : arr)
+	{
+		cout << ele;
+	}
+
+	cout << "Size of the array is: ";
+	cout << sizeof(arr)/sizeof(int);
+
+	string someVar = "someVar is string there";
+	string &soeVar_ = someVar;
+	cout << soeVar_;
+
+	string* po = &someVar;
+	cout << *po; //De reference
+
+	*po = "New value now!";
+	cout << someVar;
+	cout << *po;
+	int aa = 1;
+	int bb = 7;
+	SwapNos(aa, bb);
+	cout << aa << endl << bb;
+
+	Fish fish(1, 3, 6);
+	fish.height = 6;
+	fish.length = 1;
+	// fish.weight = 1;
+	// fish.shape = "shape is":
+	cout << fish.height << " " << fish.length << " " << fish.Move(Left);
+
+	Cichlid cichlid;
+	cout << cichlid.height << " " << cichlid.length;
+
+	ofstream SomeFile("NameFile");
+	SomeFile << "Some content to the file!";
+	SomeFile.close();
+}
